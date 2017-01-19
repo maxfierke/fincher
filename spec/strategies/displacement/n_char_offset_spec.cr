53a154a41d9ee01f1c0d431f7a2f5cfafe4a27f3
Max Fierke <max@maxfierke.com>
2017-01-18 20:35:51 -0600

    Add NCharOffset displacement strategy spec

---
+++ spec/strategies/displacement/n_char_offset_spec.cr
@@ -0,0 +1,36 @@
+require "../../spec_helper"
+
+describe Typhar::DisplacementStrategies::NCharOffset do
+  describe "#advance_to_next!" do
+    scanner = StringScanner.new("hello")
+
+    describe "when the displacement is feasible" do
+      n_char_offsetter = Typhar::DisplacementStrategies::NCharOffset.new(
+        scanner,
+        123.to_u32,
+        10
+      )
+      source_text_scanner = StringScanner.new("lorem ipsum test")
+
+      it "adds the configured offset to the StringScanner#offset" do
+        n_char_offsetter.advance_to_next!(source_text_scanner)
+        source_text_scanner.offset = 10
+      end
+    end
+
+    describe "when the displacement is not feasible" do
+      n_char_offsetter = Typhar::DisplacementStrategies::NCharOffset.new(
+        scanner,
+        123.to_u32,
+        10
+      )
+      source_text_scanner = StringScanner.new("lorem")
+
+      it "raises an exception" do
+        expect_raises(Typhar::StrategyNotFeasibleException) do
+          n_char_offsetter.advance_to_next!(source_text_scanner)
+        end
+      end
+    end
+  end
+end
